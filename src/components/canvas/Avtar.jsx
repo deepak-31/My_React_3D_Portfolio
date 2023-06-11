b13e01885034baf4221a5cc650eeda6beea8feab
deepak-31 <deepak310896@gmail.com>
2023-06-11 14:16:11 +0530

    code updated with new 3d model
    code updated with new 3d model

---
+++ src/components/canvas/Avtar.jsx
@@ -0,0 +1,78 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const MyAvtarWork = ({ isMobile }) => {
+    const myAvtar = useGLTF("./models/646d9dcdc8a5f5bddbfac913.glb");
+
+    return (
+        <mesh>
+            <hemisphereLight intensity={1} groundColor='black' />
+            <spotLight
+                position={[50, 50, 10]}
+                angle={0.9}
+                penumbra={1}
+                intensity={1}
+                castShadow
+                shadow-mapSize={1024}
+            />
+            <pointLight intensity={1} />
+            <primitive
+                object={myAvtar.scene}
+                scale={isMobile ? 8 : 10}
+                position={isMobile ? [0, -10, 0] : [0, -6, 0]}
+                rotation={[0, 0.7, 0]}
+            />
+        </mesh>
+    );
+};
+
+const Avtar = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Add a listener for changes to the screen size
+        const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+        // Set the initial value of the `isMobile` state variable
+        setIsMobile(mediaQuery.matches);
+
+        // Define a callback function to handle changes to the media query
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        // Add the callback function as a listener for changes to the media query
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        // Remove the listener when the component is unmounted
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
+
+    return (
+        <Canvas
+            frameloop='demand'
+            shadows
+            dpr={[1, 2]}
+            // camera={{ position: [20, 0, 50], fov: 50 }}
+            camera={{ position: [30, 30, 30], fov: 30 }}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <MyAvtarWork isMobile={isMobile} />
+            </Suspense>
+            <Preload all />
+        </Canvas>
+    );
+};
+
+export default Avtar;
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,5 +2,6 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
+import Avtar from "./Avtar"
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, Avtar };
